27e976078a8cd88de31f630d05faf2956e94a317
Nguyen Thuong Thong <nguyenthuongthongcm@gmail.com>
2018-05-24 23:23:47 +0700

    Init Commit

--- City Searcher/ViewController.swift
+++ City Searcher/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
+    var showCities = [String]() // Data source for UITableView
+    let allCities = ["New York", "London", "Oslo", "Warsaw", "Berlin", "Praga"] // Our mocked API data source
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        searchBar
+            .rx.text // Observable property thanks to RxCocoa
+            .orEmpty // Make it non-optional
+            .debounce(0.5, scheduler: MainScheduler.instance) // Wait 0.5 for changes.
+            .distinctUntilChanged() // If they didn't occur, check if the new value is the same as old.
+            .filter { !$0.isEmpty } // Filter for non-empty query
+            .subscribe(onNext: { [weak self] value in // Here we subscribe to every new value
+                self?.showCities = (self?.allCities.filter{ $0.hasPrefix(value) })! // We now do our "API Request" to find cities
+                self?.tableView.reloadData() // And reload table view data
+            })
+            .disposed(by: disposeBag)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return showCities.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cityPrototypeCell", for: indexPath)
+        cell.textLabel?.text = showCities[indexPath.row]
+        
+        return cell
+    }
 
 }
 
